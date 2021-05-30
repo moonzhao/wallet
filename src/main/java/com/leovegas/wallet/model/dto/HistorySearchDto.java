3cd527ff3138ae8d89ca7ff6a632ed4a0c798e4c
zhongyu.zhao <ezaozyu@gmail.com>
2021-05-30 04:00:01 -0700

    add @JsonProperty(required = true) to dto

--- src/main/java/com/leovegas/wallet/model/dto/HistorySearchDto.java
+++ src/main/java/com/leovegas/wallet/model/dto/HistorySearchDto.java
@@ -1,5 +1,6 @@
 package com.leovegas.wallet.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -8,8 +9,11 @@ import java.time.LocalDate;
 @Data
 public class HistorySearchDto {
 
+    @JsonProperty(required = true)
     @NotBlank(message = "accountId is mandatory")
     private String accountId;
+
     private LocalDate begin;
+
     private LocalDate end;
 }
--- src/main/java/com/leovegas/wallet/model/dto/TransactionDto.java
+++ src/main/java/com/leovegas/wallet/model/dto/TransactionDto.java
@@ -1,5 +1,6 @@
 package com.leovegas.wallet.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.leovegas.wallet.model.entity.AccountEntity;
 import com.leovegas.wallet.model.entity.TransactionEntity;
 import lombok.Builder;
@@ -14,10 +15,15 @@ import java.time.LocalDateTime;
 @Builder
 public class TransactionDto {
 
+    @JsonProperty(required = true)
     @NotBlank(message = "transactionId is mandatory")
     private String transactionId;
+
+    @JsonProperty(required = true)
     @NotBlank(message = "accountId is mandatory")
     private String accountId;
+
+    @JsonProperty(required = true)
     @NotNull(message = "amount is mandatory")
     @Min(value = 0L, message = "The value must be positive")
     private double amount;
